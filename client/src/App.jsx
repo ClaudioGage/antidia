1a4d354049beebcf5ab2b9235edffc4911ba2889
ClaudioGage <gageclaudio@gmail.com>
2019-05-17 12:46:41 -0500

    restructuring boilerplate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
-// RHL only for front end development
-import { hot } from 'react-hot-loader';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return <div>Welcome to a Modern Minimal React Boilerplate</div>;
   }
 }
-// hot export works with RHL. Remove line 11 when starting fullstack integration
-export default hot(module)(App);
-// Uncomment line 13 & delete line 11 when starting fullstack integration
-// export default App;
+
+export default App;
