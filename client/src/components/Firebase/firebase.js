626ad464832c52fa9ed2a298f0c26aa3afbe7aa4
ClaudioGage <gageclaudio@gmail.com>
2019-05-23 16:09:48 -0500

    integrated the Authentification API for Firebase

--- client/src/components/Firebase/firebase.js
+++ client/src/components/Firebase/firebase.js
@@ -2,6 +2,7 @@ import app from 'firebase/app';
 import {
   AUTH_CONFIG
 } from "/home/hc-39/Documents/Antidia/env.js";
+import 'firebase/auth';
 
 const config = {
   apiKey: AUTH_CONFIG.apiKey,
@@ -14,7 +15,26 @@ const config = {
 class Firebase {
   constructor() {
     app.initializeApp(config);
+    //start auth
+    this.auth = app.auth();
   }
+  /*Auth API */
+
+  //  methods to iniate registration/signup taking the email parameters
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  // methods to iniate login/signin taking the email parameters
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  // methods to change and reset the password
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password =>
+    this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
