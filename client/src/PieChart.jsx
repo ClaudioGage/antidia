855e2f2e230cea3e6af0b5ab7da29548497ee04c
ClaudioGage <gageclaudio@gmail.com>
2019-06-03 18:20:20 -0500

    modifying the pie chart component for specific results

--- client/src/PieChart.jsx
+++ client/src/PieChart.jsx
@@ -1,35 +1,69 @@
 import React, { Component } from "react";
-import { Doughnut } from "react-chartjs-2";
+import { Doughnut, defaults } from "react-chartjs-2";
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+defaults.global.mantianAspectRatio = false;
+const l = 10;
 
-const getState = {
+const State = {
   labels: [">80", "80-180", "188>"],
   datasets: [
     {
-      data: [
-        getRandomInt(50, 200),
-        getRandomInt(100, 150),
-        getRandomInt(150, 250)
-      ],
-      backgroundColor: ["#CCC", "#36A2EB", "#FFCE56"],
-      hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"]
+      data: [l, 70, 20],
+      backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+      hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+      borderColor: "#fff",
+      borderWidth: 1,
+      hoverBorderColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+      hoverBorderWidth: 8
     }
   ]
+};
+
+const options = {
+  responsive: true,
+  legend: {
+    display: true
+  },
+  animation: {
+    animateScale: true,
+    animateRotate: true
+  },
+  tooltips: {
+    displayColors: true,
+    callbacks: {
+      label: function(tooltipItem, data) {
+        //get the concerned dataset
+        var dataset = data.datasets[tooltipItem.datasetIndex];
+        //calculate the total of this data set
+        var total = dataset.data.reduce(function(
+          previousValue,
+          currentValue,
+          currentIndex,
+          array
+        ) {
+          return previousValue + currentValue;
+        });
+        //get the current items value
+        var currentValue = dataset.data[tooltipItem.index];
+        //calculate the precentage based on the total and current item, also this does a rough rounding to give a whole number
+        var percentage = Math.floor((currentValue / total) * 100 + 0.5);
+
+        return percentage + "%";
+      }
+    }
+  }
 };
 
 export default class PieChart extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { ...getState };
+    this.state = { ...State };
   }
   componentWillMount() {
     setInterval(() => {
       this.setState({
-        ...getState
+        ...State
       });
     }, 5000);
   }
@@ -38,32 +72,8 @@ export default class PieChart extends Component {
     return (
       <div>
         <h2>Glucose range average</h2>
-        <Doughnut data={this.state} />
+        <Doughnut data={this.state} options={options} />
       </div>
     );
   }
 }
-/*
-export default React.createClass({
-  displayName: "DynamicDoughnutExample",
-
-  getInitialState() {
-    return getState();
-  },
-
-  componentWillMount() {
-    setInterval(() => {
-      this.setState(getState());
-    }, 5000);
-  },
-
-  render() {
-    return (
-      <div>
-        <h2>Dynamicly refreshed Doughnut Example</h2>
-        <Doughnut data={this.state} />
-      </div>
-    );
-  }
-});
-*/
